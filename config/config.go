53c49cd0f158f242c45fdb6d82b69ad94e633325
hovanduan <duan.ho-van@banvien.com.vn>
2024-11-22 13:45:56 +0700

    x

---
+++ config/config.go
@@ -0,0 +1,48 @@
+package configs
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+var (
+	GlobalConfig = &Configuration{}
+)
+
+func init() {
+	viper.AddConfigPath("./configs")
+	viper.SetConfigName("config")
+	viper.SetConfigType("env")
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Can't read config file: %v", err)
+		return
+	}
+
+	config := &Configuration{}
+	err = viper.Unmarshal(config)
+	if err != nil {
+		log.Fatalf("Can't unmarshal config: %v", err)
+		return
+	}
+
+	GlobalConfig = config
+
+	fmt.Println("\033[34m================= Loaded Configuration =================\033[0m")
+	fmt.Printf("\033[36mPort:            \033[32m%d\033[0m\n", GlobalConfig.Port)
+	fmt.Printf("\033[36mGRPC Task Host:  \033[32m%s\033[0m\n", GlobalConfig.GrpcTaskHost)
+	fmt.Printf("\033[36mGRPC Task Port:  \033[32m%d\033[0m\n", GlobalConfig.GrpcTaskPort)
+	fmt.Println("\033[34m========================================================\033[0m")
+
+	return
+}
+
+type Configuration struct {
+	Port         int    `mapstructure:"PORT"`
+	GrpcTaskHost string `mapstructure:"GRPC_TASK_HOST"`
+	GrpcTaskPort int    `mapstructure:"GRPC_TASK_PORT"`
+}
---
+++ server/route/grpc/task.go
@@ -0,0 +1,26 @@
+package grpc
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/ngdangkietswe/swe-gateway-service/configs"
+	gen "github.com/ngdangkietswe/swe-protobuf-shared/generated/task"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func RegisterTaskGrpcHandler(mux *runtime.ServeMux) {
+	err := gen.RegisterTaskServiceHandlerFromEndpoint(
+		context.Background(),
+		mux,
+		fmt.Sprintf("%s:%d", configs.GlobalConfig.GrpcTaskHost, configs.GlobalConfig.GrpcTaskPort),
+		[]grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())},
+	)
+
+	if err != nil {
+		log.Fatalf("Can't register Task GRPC Handler: %v", err)
+	}
+}
